de3edffc365ca98f20351daff2bba45d76bd4a50
罗国强 <luogq@34.39.17.218.broad.sz.gd.dynamic.163data.com.cn>
2019-03-07 17:56:37 +0800

    添加新文件

---
+++ WebView_cell/WebView/TestViewController.h
@@ -0,0 +1,17 @@
+//
+//  TestViewController.h
+//  WebView
+//
+//  Created by 罗国强 on 2019/3/7.
+//  Copyright © 2019年 JKLazyCat. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface TestViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ WebView_cell/WebView/TestViewController.m
@@ -0,0 +1,33 @@
+
+//
+//  TestViewController.m
+//  WebView
+//
+//  Created by 罗国强 on 2019/3/7.
+//  Copyright © 2019年 JKLazyCat. All rights reserved.
+//
+
+#import "TestViewController.h"
+
+@interface TestViewController ()
+
+@end
+
+@implementation TestViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
